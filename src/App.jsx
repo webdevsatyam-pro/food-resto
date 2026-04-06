990be8e7f1c04e4f596a334cd0fcde195852ff0c
webdevsatyam-pro <satyam143142@gmail.com>
2026-04-06 13:25:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+
+const App = () => {
+  return <div className="bg-amber-400">App</div>;
+};
+
+<Routes>
+  <Route />
+</Routes>;
+export default App;
